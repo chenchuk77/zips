68d13f4f84ae9dc11b58294ba6deeda2508925d9
chenchuk <chenchuk@gmail.com>
2018-10-15 20:33:51 +0300

    adding dao model

---
+++ app/models/zip.rb
@@ -0,0 +1,15 @@
+class Zip
+
+  # easy console access
+  def self.mongo_client
+  	Mongoid::Clients.default
+  end
+
+  # easy access to zip collection
+  def self.collection
+  	self.mongo_client[:zips]
+  end
+
+
+
+end
